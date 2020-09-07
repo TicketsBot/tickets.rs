300dc6d662d4cc025758fb8b12b33934ac7464be
rxdn <29165304+rxdn@users.noreply.github.com>
2020-09-07 20:34:41 +0100

    fix flag

--- sharder/src/gateway/shard.rs
+++ sharder/src/gateway/shard.rs
@@ -527,7 +527,6 @@ impl Shard {
                 {
                     let last_ack = *self.last_ack.read().await;
                     let last_heartbeat = *self.last_heartbeat.read().await;
-                    println!("{:?} {:?}", last_ack, last_heartbeat);
                     if has_sent_heartbeat && (last_heartbeat > last_ack || last_ack.duration_since(last_heartbeat) > interval) {
                         // BIG problem
                         // TODO: panic?
