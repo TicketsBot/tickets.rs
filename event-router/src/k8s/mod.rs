ee847e44809155c877842616075baf40370f5224
rxdn <29165304+rxdn@users.noreply.github.com>
2021-07-12 17:58:07 +0100

    Remove event-router

--- event-router/src/k8s/mod.rs
+++
@@ -1,8 +0,0 @@
-mod client;
-pub use client::KubernetesClient;
-
-mod pod;
-pub use pod::*;
-
-mod pod_update;
-pub use pod_update::{PodUpdate, PodData};
--- event-router/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod error;
-pub use error::RouterError;
-
-mod k8s;
-mod pool;
